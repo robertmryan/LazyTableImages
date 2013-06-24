08c517fd8675018c5a92af3102d18dddf213c562
robertmryan <robert.ryan@mindspring.com>
2013-06-24 17:03:50 -0400

    Make IconDownloader a `NSOperation` subclass and eliminate an unnecessary property (`NSOperation` already has a `completionBlock` property) as well as two methods (`startDownload` and `cancelDownload` are, in conformance with `NSOperation`, now simply `start` and `cancel`, respectively).

--- Classes/IconDownloader.h
+++ Classes/IconDownloader.h
@@ -51,12 +51,8 @@
 @class AppRecord;
 
 
-@interface IconDownloader : NSObject
+@interface IconDownloader : NSOperation
 
 @property (nonatomic, strong) AppRecord *appRecord;
-@property (nonatomic, copy) void (^completionHandler)(void);
-
-- (void)startDownload;
-- (void)cancelDownload;
 
 @end
